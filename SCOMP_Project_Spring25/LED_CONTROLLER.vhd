ee1a013c20039a3a99611cb9ce6116dbb9bece1c
zlin415 <zlin415@gatech.edu>
2025-04-01 22:31:39 -0400

    Made some changes, modified bdf to use current controller, controller is now under a single clock edge, attempted gamma correction (untested)

--- SCOMP_Project_Spring25/LED_CONTROLLER.vhd
+++ SCOMP_Project_Spring25/LED_CONTROLLER.vhd
@@ -17,7 +17,7 @@ ENTITY LED_CONTROLLER IS
 PORT(
     CS          : IN  STD_LOGIC;
     WRITE_EN    : IN  STD_LOGIC;
-	 CLOCK       : IN  STD_LOGIC;
+	 CLOCK       : IN  STD_LOGIC; -- 10khz = 10000 cycles per second
     RESETN      : IN  STD_LOGIC;
     LEDs        : OUT STD_LOGIC_VECTOR(9 DOWNTO 0);
     IO_DATA     : IN  STD_LOGIC_VECTOR(15 DOWNTO 0)
@@ -29,7 +29,7 @@ ARCHITECTURE a OF LED_CONTROLLER IS --added by henry
     SIGNAL mode : STD_LOGIC;
     SIGNAL brightness_select : STD_LOGIC_VECTOR(4 DOWNTO 0);
     SIGNAL led_select : STD_LOGIC_VECTOR(9 DOWNTO 0);
-	 SIGNAL counter_uniform: natural range 0 to 10 := 0; 
+	 SIGNAL counter_uniform: natural range 0 to 199 := 0; 
 	 SIGNAL PULSE : STD_LOGIC;
 	 signal count   : std_logic_vector(15 downto 0);  -- internal counter
 
@@ -40,8 +40,12 @@ BEGIN
         IF (RESETN = '0') THEN
             -- Turn off LEDs at reset (a nice usability feature)
             LEDs <= "0000000000";
-        ELSIF (RISING_EDGE(CS)) THEN
-            IF WRITE_EN = '1' THEN
+				count <= x"0000";
+				mode <= '0';
+				brightness_select <= (others => '0');
+				led_select <= (others => '0');
+        ELSIF (RISING_EDGE(CLOCK)) THEN
+            IF WRITE_EN = '1' and CS = '1' THEN
                 -- If SCOMP is sending data to this peripheral,
                 -- use that data directly to determine the modes,
                      --brightness and led selection.
@@ -49,57 +53,54 @@ BEGIN
 					 mode <= IO_DATA(15); --added by henry 
                 brightness_select <= IO_DATA(14 DOWNTO 10);
                 led_select <= IO_DATA(9 DOWNTO 0);
-                
+            end if;    
 					 
-					IF mode = '0' THEN
-						--code here for pairing mode
+				IF mode = '0' THEN
+					--code here for pairing mode
 						
-						end if;
 						
 
-					ELSIF mode = '1' THEN
-						--code here for uniform mode
-						IF brightness_select(0) = '1' then
-							counter_uniform <= 10;
-						ELSIF (brightness_select & "00010") = "00010" then
-							counter_uniform <= 8;
-						ELSIF (brightness_select & "00100") = "00100" then
-							counter_uniform <= 6;
-						ELSIF (brightness_select & "01000") = "01000" then
-							counter_uniform <= 4;
-						ELSIF (brightness_select & "10000") = "10000" then
-							counter_uniform <= 2;
-						ELSE
-							counter_uniform <= 0;
-						END IF;
+				ELSIF mode = '1' THEN
+					--code here for uniform mode
+					IF brightness_select(0) = '1' then
+						counter_uniform <= 199;
+					ELSIF (brightness_select & "00010") = "00010" then
+						counter_uniform <= 127;
+					ELSIF (brightness_select & "00100") = "00100" then
+						counter_uniform <= 71;
+					ELSIF (brightness_select & "01000") = "01000" then
+						counter_uniform <= 31;
+					ELSIF (brightness_select & "10000") = "10000" then
+						counter_uniform <= 7;
+					ELSE
+						counter_uniform <= 0;
+					END IF;
 						
-						if (RESETN = '0') then
-						count <= x"0000";
-						elsif rising_edge(CLOCK) then
-						-- Each clock cycle, a counter is incremented.
-						count <= count + 1;
-						end if;
-
-						-- When the counter reaches the full desired period, start the period over.
-						if count = x"00C7" then  -- 20 ms has elapsed
+					-- Each clock cycle, a counter is incremented.
+					count <= count + 1;
+					-- When the counter reaches the full desired period, start the period over.
+					if count = x"00C7" then  -- 20 ms has elapsed
 						-- Reset the counter and set the output high.
 						count <= x"0000";
 						PULSE <= '1';
 
-						-- Within the period, when the counter reaches the "command" value, set the output low.
-						-- This will make larger command values produce longer pulses.
-						elsif count = counter_uniform then
-                  PULSE <= '0';
-						end if;
-		  end if;
+					-- Within the period, when the counter reaches the "command" value, set the output low.
+					-- This will make larger command values produce longer pulses.
+					elsif count = counter_uniform then
+						PULSE <= '0';
+					ELSE
+						PULSE <= '1';
+					end if;
+						
+					if PULSE = '1' THEN 
+						LEDs <= led_select;
+					else 
+						LEDs <= (others => '0');
+					end if;
+            end if;
         end if;
-		  END PROCESS;
+    END PROCESS;
 		  
-		  PROCESS (PULSE, led_select)
-		  begin
-		  if (PULSE) THEN 
-		  LEDs <= led_select;
-        end if;
-		  end process;
 
-			END a;
+
+END a;
