c652d8d5e087df9fd1a8572b2f670a4474bba1cd
zlin415 <zlin415@gatech.edu>
2025-04-08 00:37:32 -0400

    Implemented mode 1

--- SCOMP_Project_Spring25/LED_CONTROLLER.vhd
+++ SCOMP_Project_Spring25/LED_CONTROLLER.vhd
@@ -29,7 +29,13 @@ ARCHITECTURE a OF LED_CONTROLLER IS --added by henry
     SIGNAL mode : STD_LOGIC;
     SIGNAL brightness_select : STD_LOGIC_VECTOR(4 DOWNTO 0);
     SIGNAL led_select : STD_LOGIC_VECTOR(9 DOWNTO 0);
+	 SIGNAL led_g1 : STD_LOGIC_VECTOR(9 DOWNTO 0); -- leds grouped by 33% brightness
+	 SIGNAL led_g2 : STD_LOGIC_VECTOR(9 DOWNTO 0); -- leds grouped by 67% brightness
+	 SIGNAL led_g3 : STD_LOGIC_VECTOR(9 DOWNTO 0); -- leds grouped by full brightness
 	 SIGNAL counter_uniform: natural range 0 to 199 := 0; 
+	 SIGNAL c1: natural range 0 to 199 := 0; -- for mode 0
+	 SIGNAL c2: natural range 0 to 199 := 0; 
+	 SIGNAL c3: natural range 0 to 199 := 0; -- 
 	 SIGNAL PULSE : STD_LOGIC;
 	 signal count   : std_logic_vector(15 downto 0);  -- internal counter
 
@@ -44,6 +50,9 @@ BEGIN
 				mode <= '0';
 				brightness_select <= (others => '0');
 				led_select <= (others => '0');
+				led_g1 <= (others => '0');
+				led_g2 <= (others => '0');
+				led_g3 <= (others => '0');
         ELSIF (RISING_EDGE(CLOCK)) THEN
             IF WRITE_EN = '1' and CS = '1' THEN
                 -- If SCOMP is sending data to this peripheral,
@@ -57,8 +66,118 @@ BEGIN
 					 
 				IF mode = '0' THEN
 					--code here for pairing mode
-						
-						
+					c1 <= 21; --  33% brightness, g1 brightness
+					c2 <= 89; --  67% brightness, g2 brightness
+					c3 <= 199; -- full brightness, g3 brightness
+					if brightness_select(4) = '1' then
+						if led_select(9) = '1' then
+							led_g3(9) <= '1';
+						else 
+							led_g2(9) <= '1';
+						end if;
+						if led_select(8) = '1' then
+							led_g3(8) <= '1';
+						else 
+							led_g2(8) <= '1';
+						end if;
+					else 
+						if led_select(9) = '1' then
+							led_g1(9) <= '1';
+						end if;
+						if led_select(8) = '1' then
+							led_g1(8) <= '1';
+						end if;
+					if brightness_select(3) = '1' then
+						if led_select(7) = '1' then
+							  led_g3(7) <= '1';
+						 else 
+							  led_g2(7) <= '1';
+						 end if;
+						 if led_select(6) = '1' then
+							  led_g3(6) <= '1';
+						 else 
+							  led_g2(6) <= '1';
+						 end if;
+					else 
+						 if led_select(7) = '1' then
+							  led_g1(7) <= '1';
+						 end if;
+						 if led_select(6) = '1' then
+							  led_g1(6) <= '1';
+						 end if;
+					end if;
+
+					if brightness_select(2) = '1' then
+						 if led_select(5) = '1' then
+							  led_g3(5) <= '1';
+						 else 
+							  led_g2(5) <= '1';
+						 end if;
+						 if led_select(4) = '1' then
+							  led_g3(4) <= '1';
+						 else 
+							  led_g2(4) <= '1';
+						 end if;
+					else 
+						 if led_select(5) = '1' then
+							  led_g1(5) <= '1';
+						 end if;
+						 if led_select(4) = '1' then
+							  led_g1(4) <= '1';
+						 end if;
+					end if;
+
+					if brightness_select(1) = '1' then
+						 if led_select(3) = '1' then
+							  led_g3(3) <= '1';
+						 else 
+							  led_g2(3) <= '1';
+						 end if;
+						 if led_select(2) = '1' then
+							  led_g3(2) <= '1';
+						 else 
+							  led_g2(2) <= '1';
+						 end if;
+					else 
+						 if led_select(3) = '1' then
+							  led_g1(3) <= '1';
+						 end if;
+						 if led_select(2) = '1' then
+							  led_g1(2) <= '1';
+						 end if;
+					end if;
+
+					if brightness_select(0) = '1' then
+						 if led_select(1) = '1' then
+							  led_g3(1) <= '1';
+						 else 
+							  led_g2(1) <= '1';
+						 end if;
+						 if led_select(0) = '1' then
+							  led_g3(0) <= '1';
+						 else 
+							  led_g2(0) <= '1';
+						 end if;
+					else 
+						 if led_select(1) = '1' then
+							  led_g1(1) <= '1';
+						 end if;
+						 if led_select(0) = '1' then
+							  led_g1(0) <= '1';
+						 end if;
+					end if;
+					-- Each clock cycle, a counter is incremented.
+					count <= count + 1;	
+					if count = x"00C7" then  -- 20 ms has elapsed
+						-- Reset the counter and set the output high.
+						count <= x"0000";
+					if count < c1 then
+						LEDs <= led_g1 or led_g2 or led_g3;
+					elsif count < c2 then
+						LEDs <= led_g2 or led_g3;
+					else
+						LEDs <= led_g3;
+					end if;
 
 				ELSIF mode = '1' THEN
 					--code here for uniform mode
