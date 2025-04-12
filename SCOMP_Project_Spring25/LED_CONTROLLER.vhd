02d99a5e518d5fd28a6696736fbd0be091911e54
gquan3 <adrian.kalisz@outlook.com>
2025-04-12 18:23:12 -0400

    The updated version after Saturday

--- SCOMP_Project_Spring25/LED_CONTROLLER.vhd
+++ SCOMP_Project_Spring25/LED_CONTROLLER.vhd
@@ -42,6 +42,7 @@ ARCHITECTURE a OF LED_CONTROLLER IS --added by henry
 
 BEGIN
     PROCESS (RESETN, CS, CLOCK)
+	 -- POTENTIAL ISSUE "YOU CAN'T SET AND USE A VALUE AT THE SAME TIME"!!!
     BEGIN
         IF (RESETN = '0') THEN
             -- Turn off LEDs at reset (a nice usability feature)
@@ -54,7 +55,7 @@ BEGIN
 				led_g2 <= (others => '0');
 				led_g3 <= (others => '0');
         ELSIF (RISING_EDGE(CLOCK)) THEN
-            IF WRITE_EN = '1' and CS = '1' THEN
+            IF WRITE_EN = '1' and CS = '1' THEN -- if1
                 -- If SCOMP is sending data to this peripheral,
                 -- use that data directly to determine the modes,
                      --brightness and led selection.
@@ -62,115 +63,220 @@ BEGIN
 					 mode <= IO_DATA(15); --added by henry 
                 brightness_select <= IO_DATA(14 DOWNTO 10);
                 led_select <= IO_DATA(9 DOWNTO 0);
-            end if;    
+            end if; -- endif1
 					 
-				IF mode = '0' THEN
+				IF mode = '0' THEN -- if2
 					--code here for pairing mode
 					c1 <= 21; --  33% brightness, g1 brightness
 					c2 <= 89; --  67% brightness, g2 brightness
 					c3 <= 199; -- full brightness, g3 brightness
-					if brightness_select(4) = '1' then
-						if led_select(9) = '1' then
+					if brightness_select(4) = '1' then -- if3
+						if led_select(9) = '1' then  -- if4
 							led_g3(9) <= '1';
+							led_g2(9) <= '0';
+							led_g1(9) <= '0';
 						else 
+							led_g3(9) <= '0';
 							led_g2(9) <= '1';
-						end if;
-						if led_select(8) = '1' then
+							led_g1(9) <= '0';
+						end if; -- endif4
+						if led_select(8) = '1' then -- if5
 							led_g3(8) <= '1';
-						else 
+							led_g2(8) <= '0';
+							led_g1(8) <= '0';
+						else
+							led_g3(8) <= '0';
 							led_g2(8) <= '1';
-						end if;
+							led_g1(8) <= '0';
+						end if; -- endif5
 					else 
-						if led_select(9) = '1' then
+						if led_select(9) = '1' then -- if6
+							led_g3(9) <= '0';
+							led_g2(9) <= '0';
 							led_g1(9) <= '1';
-						end if;
-						if led_select(8) = '1' then
+						else
+							led_g3(9) <= '0';
+							led_g2(9) <= '0';
+							led_g1(9) <= '0';
+						end if; -- endif6
+						if led_select(8) = '1' then -- if7
+							led_g3(8) <= '0';
+							led_g2(8) <= '0';
 							led_g1(8) <= '1';
-						end if;
-					if brightness_select(3) = '1' then
-						if led_select(7) = '1' then
+						else
+							led_g3(8) <= '0';
+							led_g2(8) <= '0';
+							led_g1(8) <= '0';
+						end if; -- endif7
+					end if;
+					
+					if brightness_select(3) = '1' then -- if8
+						if led_select(7) = '1' then -- if9
 							  led_g3(7) <= '1';
+							  led_g2(7) <= '0';
+							  led_g1(7) <= '0';
 						 else 
+							  led_g3(7) <= '0';
 							  led_g2(7) <= '1';
+							  led_g1(7) <= '0';
 						 end if;
 						 if led_select(6) = '1' then
 							  led_g3(6) <= '1';
-						 else 
+							  led_g2(6) <= '0';
+							  led_g1(6) <= '0';
+						 else
+							  led_g3(6) <= '0';
 							  led_g2(6) <= '1';
+							  led_g3(6) <= '0';
 						 end if;
-					else 
-						 if led_select(7) = '1' then
-							  led_g1(7) <= '1';
-						 end if;
-						 if led_select(6) = '1' then
-							  led_g1(6) <= '1';
-						 end if;
+					else
+						if led_select(7) = '1' then
+							led_g3(7) <= '0';
+							led_g2(7) <= '0';
+							led_g1(7) <= '1';
+						else
+							led_g3(7) <= '0';
+							led_g2(7) <= '0';
+							led_g1(7) <= '0';
+						end if;
+						if led_select(6) = '1' then
+							led_g3(6) <= '0';
+							led_g2(6) <= '0';
+							led_g1(6) <= '1';
+						else
+							led_g3(6) <= '0';
+							led_g2(6) <= '0';
+							led_g1(6) <= '0';
+						end if;
 					end if;
 
 					if brightness_select(2) = '1' then
-						 if led_select(5) = '1' then
-							  led_g3(5) <= '1';
-						 else 
-							  led_g2(5) <= '1';
-						 end if;
-						 if led_select(4) = '1' then
-							  led_g3(4) <= '1';
-						 else 
-							  led_g2(4) <= '1';
-						 end if;
+						if led_select(5) = '1' then
+							led_g3(5) <= '1';
+							led_g2(5) <= '0';
+							led_g1(5) <= '0';
+						else 
+							led_g3(5) <= '0';
+							led_g2(5) <= '1';
+							led_g1(5) <= '0';
+						end if;
+						if led_select(4) = '1' then
+							led_g3(4) <= '1';
+							led_g2(4) <= '0';
+							led_g1(4) <= '0';
+						else 
+							led_g3(4) <= '0';
+							led_g2(4) <= '1';
+							led_g1(4) <= '0';
+						end if;
 					else 
-						 if led_select(5) = '1' then
-							  led_g1(5) <= '1';
-						 end if;
-						 if led_select(4) = '1' then
-							  led_g1(4) <= '1';
-						 end if;
+						if led_select(5) = '1' then
+							led_g3(5) <= '0';
+							led_g2(5) <= '0';
+							led_g1(5) <= '1';
+						else
+							led_g3(5) <= '0';
+							led_g2(5) <= '0';
+							led_g1(5) <= '0';
+						end if;
+						if led_select(4) = '1' then
+							led_g3(4) <= '0';
+							led_g2(4) <= '0';
+							led_g1(4) <= '1';
+						else
+							led_g3(4) <= '0';
+							led_g2(4) <= '0';
+							led_g1(4) <= '0';
+						end if;
 					end if;
 
 					if brightness_select(1) = '1' then
-						 if led_select(3) = '1' then
-							  led_g3(3) <= '1';
-						 else 
-							  led_g2(3) <= '1';
-						 end if;
-						 if led_select(2) = '1' then
-							  led_g3(2) <= '1';
-						 else 
-							  led_g2(2) <= '1';
-						 end if;
+						if led_select(3) = '1' then
+							led_g3(3) <= '1';
+							led_g2(3) <= '0';
+							led_g1(3) <= '0';
+						else 
+							led_g3(3) <= '0';
+							led_g2(3) <= '1';
+							led_g1(3) <= '0';
+						end if;
+						if led_select(2) = '1' then
+							led_g3(2) <= '1';
+							led_g2(2) <= '0';
+							led_g1(2) <= '0';
+						else 
+							led_g3(2) <= '0';
+							led_g2(2) <= '1';
+							led_g1(2) <= '0';
+						end if;
 					else 
-						 if led_select(3) = '1' then
-							  led_g1(3) <= '1';
-						 end if;
-						 if led_select(2) = '1' then
-							  led_g1(2) <= '1';
-						 end if;
+						if led_select(3) = '1' then
+							led_g3(3) <= '0';
+							led_g2(3) <= '0';
+							led_g1(3) <= '1';
+						else
+							led_g3(3) <= '0';
+							led_g2(3) <= '0';
+							led_g1(3) <= '0';
+						end if;
+						if led_select(2) = '1' then
+							led_g3(2) <= '0';
+							led_g2(2) <= '0';
+							led_g1(2) <= '1';
+						else
+							led_g3(2) <= '0';
+							led_g2(2) <= '0';
+							led_g1(2) <= '0';
+						end if;
 					end if;
 
 					if brightness_select(0) = '1' then
-						 if led_select(1) = '1' then
-							  led_g3(1) <= '1';
-						 else 
-							  led_g2(1) <= '1';
-						 end if;
-						 if led_select(0) = '1' then
-							  led_g3(0) <= '1';
-						 else 
-							  led_g2(0) <= '1';
-						 end if;
+						if led_select(1) = '1' then
+							led_g3(1) <= '1';
+							led_g2(1) <= '0';
+							led_g1(1) <= '0';
+						else 
+							led_g3(1) <= '0';
+							led_g2(1) <= '1';
+							led_g1(1) <= '0';
+						end if;
+						if led_select(0) = '1' then
+							led_g3(0) <= '1';
+							led_g2(0) <= '0';
+							led_g1(0) <= '0';
+						else 
+							led_g3(0) <= '0';
+							led_g2(0) <= '1';
+							led_g1(0) <= '0';
+						end if;
 					else 
-						 if led_select(1) = '1' then
-							  led_g1(1) <= '1';
-						 end if;
-						 if led_select(0) = '1' then
-							  led_g1(0) <= '1';
-						 end if;
+						if led_select(1) = '1' then
+							led_g3(1) <= '0';
+							led_g2(1) <= '0';
+							led_g1(1) <= '1';
+						else
+							led_g3(1) <= '0';
+							led_g2(1) <= '0';
+							led_g1(1) <= '0';
+						end if;
+						if led_select(0) = '1' then
+							led_g3(0) <= '0';
+							led_g2(0) <= '0';
+							led_g1(0) <= '1';
+						else
+							led_g3(0) <= '0';
+							led_g2(0) <= '0';
+							led_g1(0) <= '0';
+						end if;
 					end if;
+					
+					
 					-- Each clock cycle, a counter is incremented.
 					count <= count + 1;	
 					if count = x"00C7" then  -- 20 ms has elapsed
 						-- Reset the counter and set the output high.
 						count <= x"0000";
+					end if;
 					if count < c1 then
 						LEDs <= led_g1 or led_g2 or led_g3;
 					elsif count < c2 then
